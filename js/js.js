a4f85137198c89da833be01f4710bc9afba31516
tqi969181 <58447691+tqi969181@users.noreply.github.com>
2019-12-04 13:00:55 +0000

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,124 @@
+$("#pic1").click(function(){
+	$(".mainblack").attr("src","img/img/bt.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic2").click(function(){
+	$(".mainblack").attr("src","img/img/jsy1.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic3").click(function(){
+	$(".mainblack").attr("src","img/img/jsy2.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic4").click(function(){
+	$(".mainblack").attr("src","img/img/jsy3.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic5").click(function(){
+	$(".mainblack").attr("src","img/img/fl.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic6").click(function(){
+	$(".mainblack").attr("src","img/img/mj.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic7").click(function(){
+	$(".mainblack").attr("src","img/img/xn.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic8").click(function(){
+	$(".mainblack").attr("src","img/img/rh.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic9").click(function(){
+	$(".mainblack").attr("src","img/img/om.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic10").click(function(){
+	$(".mainblack").attr("src","img/img/xzt.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic11").click(function(){
+	$(".mainblack").attr("src","img/img/cc.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic12").click(function(){
+	$(".mainblack").attr("src","img/img/t1.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic13").click(function(){
+	$(".mainblack").attr("src","img/img/t2.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic14").click(function(){
+	$(".mainblack").attr("src","img/img/t3.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic15").click(function(){
+	$(".mainblack").attr("src","img/img/t4.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic16").click(function(){
+	$(".mainblack").attr("src","img/img/t5.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic17").click(function(){
+	$(".mainblack").attr("src","img/img/t7.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic18").click(function(){
+	$(".mainblack").attr("src","img/img/t8.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic19").click(function(){
+	$(".mainblack").attr("src","img/img/t9.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$("#pic20").click(function(){
+	$(".mainblack").attr("src","img/img/t10.jpg");
+	$(".black").toggle(100);
+	$(".main").fadeOut(100);
+})
+
+$(".mainblack").click(function(){
+	$(".black").fadeOut(100);
+	$(".main").toggle(100);
+})
